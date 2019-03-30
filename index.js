a244df1643eec7d1e141035c2123caae558190c5
Ben Wiley <therealbenwiley@gmail.com>
2019-03-30 11:31:58 -0400

    Use data dir by default and ignore data dir in git.

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const path = require('path')
 const fa = require('fair-analytics')
+const mkdirp = require('mkdirp')
 
 const datadir = process.env.DATADIR || path.join(__dirname, 'data')
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 6978
+
+mkdirp.sync(datadir)
 
 const app = fa({
   storageDirectory: path.resolve(datadir),
-  memory: (process.env.NODE_ENV !== 'production')
+  memory: (process.env.NODE_ENV === 'development')
 })
 const { feed } = app
 
